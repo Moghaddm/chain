cacb6258647092f6df7a29ef2e5a7e72c12660d5
Mohammad Mahdi <MahdiMoghaddam2005@gmail.com>
2023-10-24 22:26:09 +0330

    Add Comment Service Implement Update Product Service Implement

---
+++ src/Chain.Application/Services/CommentService.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Chain.Application.Contract.Models;
+using Chain.Application.Contract.Ports.Repositories;
+using Chain.Application.Contract.Ports.Services;
+using Chain.Domain.Entities;
+
+namespace Chain.Application.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CommentService(
+            IProductRepository productRepository,
+            ICommentRepository commentRepository,
+            IUnitOfWork unitOfWork)
+            => (_commentRepository, _productRepository, _unitOfWork) = (commentRepository, productRepository, unitOfWork);
+
+        public async Task AddOnProduct(Guid productId, CommentDto commentDto)
+        {
+            try
+            {
+                var product = await _productRepository.GetByIdAsync(productId);
+
+                Comment comment = new(commentDto.WriterAlias,
+                    commentDto.Title,
+                    commentDto.Description,
+                    commentDto.Gmail,
+                    commentDto.Suggest,
+                    commentDto.DateTimeCommented,
+                    commentDto.RateNumber);
+
+                comment.Product = product;
+
+                product.Comments.Add(comment);
+
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+
+                throw;
+            }
+        }
+
+        public async Task DeleteFromProduct(Guid id, Guid productId)
+        {
+            try
+            {
+                var product = await _productRepository.GetByIdAsync(productId);
+
+                var comment = await _commentRepository.GetByIdAsync(id);
+
+                product.Comments.Remove(comment!);
+
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+
+                throw;
+            }
+        }
+
+        public async Task UpdateOnProduct(Guid id, CommentDto commentDto)
+        {
+            try
+            {
+                var comment = await _commentRepository.GetByIdAsync(id);
+
+                comment.Update(commentDto.WriterAlias,
+                    commentDto.Title,
+                    commentDto.Description,
+                    commentDto.Gmail,
+                    commentDto.Suggest,
+                    commentDto.DateTimeCommented,
+                    commentDto.RateNumber);
+
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+
+                throw;
+            }
+        }
+
+        public async ValueTask<CommentDto> GetById(Guid id)
+        {
+            var comment = await _commentRepository.GetByIdAsync(id);
+
+            CommentDto commentDto = new(comment.WriterAlias,
+                comment.Title,
+                comment.Description,
+                comment.Gmail,
+                comment.DateTimeCommented,
+                comment.Suggest,
+                comment.VoteUps,
+                comment.VoteDowns,
+                comment.RateNumber);
+
+            return commentDto;
+        }
+
+        public async ValueTask<IEnumerable<CommentDto>> GetProductComments(Guid productId)
+        {
+            var product = await _productRepository.GetByIdAsync(productId);
+
+            return product.Comments.Select(c
+                => new CommentDto(
+                c.WriterAlias,
+                c.Title,
+                c.Description,
+                c.Gmail,
+                c.DateTimeCommented,
+                c.Suggest,
+                c.VoteUps,
+                c.VoteDowns,
+                c.RateNumber));
+        }
+    }
+}
--- src/Chain.Application/Services/ProductService.cs
+++ src/Chain.Application/Services/ProductService.cs
@@ -4,45 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Chain.Application.Contract.Ports.Repositories;
+using Chain.Application.Contract.Ports.Services;
 using Chain.Application.Interfaces;
 using Chain.Application.Models;
 using Chain.Domain.Entities;
 
-namespace Chain.Infrastructure.Services
+namespace Chain.Application.Services
 {
     public class ProductService : IProductService
     {
-        private readonly IProductRepository _productRepository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ICommentService _commentService;
+        private readonly ICompanyRepository _companyRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public ProductService(IProductRepository productRepository, IUnitOfWork unitOfWork)
-            => (_productRepository, _unitOfWork) = (productRepository, unitOfWork);
+        public ProductService(
+            IProductRepository productRepository,
+            IUnitOfWork unitOfWork,
+            ICategoryRepository categoryRepository,
+            ICompanyRepository companyRepository,
+            ICommentService commentService)
+            => (_productRepository, _unitOfWork, _categoryRepository, _companyRepository, _commentService) 
+                = (productRepository, unitOfWork, categoryRepository, companyRepository, commentService);
 
-        public async Task<Guid> Create(ProductDto productDto)
+        public async Task<Guid> Create(ProductDto productDto, Guid companyId, Guid categoryId)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var cancellationToken = cancellationTokenSource.Token;
-
             try
             {
+                var category = await _categoryRepository.GetByIdAsync(categoryId);
+
+                var company = await _companyRepository.GetByIdAsync(companyId);
+
                 Product product = new(productDto.Name,
                     productDto.FullEnglishName,
                     productDto.Description,
                     productDto.Price,
                     productDto.Quantity,
-                    new Company(""),
-                    new Category("", 1));
+                    company,
+                    category,
+                    productDto.Attachments);
 
                 var productCreate = await _productRepository.CreateAsync(product);
 
-                await _unitOfWork.SaveChangesAsync(cancellationToken);
+                await _unitOfWork.SaveChangesAsync();
 
                 return productCreate;
             }
             catch (Exception exception)
             {
-                cancellationTokenSource.Cancel();
-
                 Console.WriteLine(exception);
 
                 throw;
@@ -57,66 +67,66 @@ namespace Chain.Infrastructure.Services
                 p.FullEnglishName,
                 p.Description,
                 p.Quantity,
-                p.Price, p.Company, p.Category)).ToList();
+                p.Price,
+                p.Company, 
+                p.Category,
+                p.Attachments)).ToList();
         }
 
-        public async ValueTask<ProductDto> GetById(Guid id)
+        public async ValueTask<OneProductDto> GetById(Guid id)
         {
             var product = await _productRepository.GetByIdAsync(id);
 
-            var productDto = new ProductDto(
+            var comments = await _commentService.GetProductComments(id);
+
+            var productDto = new OneProductDto(
                 product.Name,
                 product.FullEnglishName,
                 product.Description,
                 product.Quantity,
                 product.Price,
                 product.Company,
-                product.Category);
+                product.Category,
+                comments,
+                product.Attachments);
 
             return productDto;
         }
 
         public async Task Delete(Guid id)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var cancellationToken = cancellationTokenSource.Token;
-
             try
             {
                 var product = await _productRepository.GetByIdAsync(id);
 
-                await _productRepository.RemoveProduct(product);
+                await _productRepository.RemoveAsync(product);
 
-                await _unitOfWork.SaveChangesAsync(cancellationToken);
+                await _unitOfWork.SaveChangesAsync();
             }
             catch (Exception exception)
             {
-                cancellationTokenSource.Cancel();
-
                 Console.WriteLine(exception);
 
                 throw;
             }
         }
 
-        public async Task Update(Guid id, ProductDto productDto)
+        public async Task Update(Guid id, OneProductDto productDto)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var cancellationToken = cancellationTokenSource.Token;
-
             try
             {
                 var product = await _productRepository.GetByIdAsync(id);
 
-                product.UpdateProduct(productDto.Name,
-                    productDto.FullEnglishName, 
-                    productDto.Description, 
-                    productDto.Price, 
-                    productDto.Quantity, 
-                    productDto.Company, 
+                product.UpdateProduct(
+                    productDto.Name,
+                    productDto.FullEnglishName,
+                    productDto.Description,
+                    productDto.Price,
+                    productDto.Quantity,
+                    productDto.Company,
                     productDto.Category);
 
-                await _unitOfWork.SaveChangesAsync(cancellationToken);
+                await _unitOfWork.SaveChangesAsync();
             }
             catch (Exception exception)
             {
